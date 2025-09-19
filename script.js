a5869a1549e91ed3c96f82d76313d63afd25f0f8
AnuragGupta181 <job4anurag11@gmail.com>
2025-09-19 07:53:27 +0530

    header footer and homepage added

---
+++ script.js
@@ -0,0 +1,8 @@
+// Toggle burger menu
+const burger = document.getElementById("burger");
+const navLinks = document.getElementById("navLinks");
+
+burger.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+  burger.classList.toggle("toggle");
+});
